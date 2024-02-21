18737b79c2a8ed0c2a54b40f45b3a81a917c342f
kuratayuto <kurata@miya-it.com>
2024-02-21 17:16:50 +0900

    feat: Tailwind成形プラグイン追加

--- src/components/VueTest.vue
+++ src/components/VueTest.vue
@@ -1,13 +1,16 @@
 <template>
-    <div id="app" class="font-fantasy text-end border-b border-b-slate-800 md:grid flex md:justify-end justify-center gap-4 p-5">
-      <div>
-        <p class="md:text-6xl text-lg tracking-widest">{{ date }}</p>
-      </div>
-      <div>
-        <p class="md:text-6xl text-lg tracking-widest">{{ dayOfWeek }}</p>
-      </div>
-      <div>
-      <p class="md:text-6xl text-lg tracking-widest">{{ clock }}</p>
+  <div
+    id="app"
+    class="flex justify-center gap-4 border-b border-b-slate-800 p-5 text-end font-fantasy md:grid md:justify-end"
+  >
+    <div>
+      <p class="text-lg tracking-widest md:text-6xl">{{ date }}</p>
+    </div>
+    <div>
+      <p class="text-lg tracking-widest md:text-6xl">{{ dayOfWeek }}</p>
+    </div>
+    <div>
+      <p class="text-lg tracking-widest md:text-6xl">{{ clock }}</p>
     </div>
   </div>
 </template>
@@ -16,9 +19,9 @@
 export default {
   data() {
     return {
-      clock: "00:00:00",
-      date: "",
-      dayOfWeek: ""
+      clock: '00:00:00',
+      date: '',
+      dayOfWeek: '',
     };
   },
   created() {
@@ -27,16 +30,19 @@ export default {
   methods: {
     tick() {
       const now = new Date();
-      this.clock = this.formatTime(now.getHours()) + ":" +
-                   this.formatTime(now.getMinutes()) + ":" +
-                   this.formatTime(now.getSeconds());
+      this.clock =
+        this.formatTime(now.getHours()) +
+        ':' +
+        this.formatTime(now.getMinutes()) +
+        ':' +
+        this.formatTime(now.getSeconds());
       this.date = `${now.getFullYear()}-${this.formatTime(now.getMonth() + 1)}-${this.formatTime(now.getDate())}`;
-      this.dayOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"][now.getDay()];
+      this.dayOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'][now.getDay()];
       setTimeout(this.tick, 1000);
     },
     formatTime(value) {
       return value < 10 ? `0${value}` : value.toString();
-    }
-  }
+    },
+  },
 };
 </script>
