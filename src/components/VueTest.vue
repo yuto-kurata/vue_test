22c8a5911dabec1e876946f1a00c910fac8e8e15
kuratayuto <kurata@miya-it.com>
2024-02-22 09:13:14 +0900

    ref: 成形プラグイン調整

--- src/components/VueTest.vue
+++ src/components/VueTest.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    id="app"
-    class="flex justify-center gap-4 border-b border-b-slate-800 p-5 text-end font-fantasy md:grid md:justify-end"
-  >
+  <div id="clock" class="flex justify-center gap-4 border-b border-b-slate-800 p-5 text-end font-fantasy md:grid md:justify-end">
     <div>
       <p class="text-lg tracking-widest md:text-6xl">{{ date }}</p>
     </div>
@@ -30,12 +27,7 @@ export default {
   methods: {
     tick() {
       const now = new Date();
-      this.clock =
-        this.formatTime(now.getHours()) +
-        ':' +
-        this.formatTime(now.getMinutes()) +
-        ':' +
-        this.formatTime(now.getSeconds());
+      this.clock = this.formatTime(now.getHours()) + ':' + this.formatTime(now.getMinutes()) + ':' + this.formatTime(now.getSeconds());
       this.date = `${now.getFullYear()}-${this.formatTime(now.getMonth() + 1)}-${this.formatTime(now.getDate())}`;
       this.dayOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'][now.getDay()];
       setTimeout(this.tick, 1000);
